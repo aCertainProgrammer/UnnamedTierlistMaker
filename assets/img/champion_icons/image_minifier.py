0e760a681c4157584efb1720a98deab7afee761f
aCertainProgrammer <aCertainProgrammer@users.noreply.github.com>
2025-01-23 17:25:12 +0000

    deploy: 819bcb5d5f6e318320218a458357072a4acc0632

--- assets/img/champion_icons/image_minifier.py
+++ assets/img/champion_icons/image_minifier.py
@@ -1,38 +1,24 @@
 import os
 from PIL import Image
 
-# Function to crop an image
-def crop_image(image_path, crop_box):
-    # Open the image file
-    with Image.open(image_path) as img:
-        # Crop the image using the provided crop box (left, upper, right, lower)
-        cropped_img = img.crop(crop_box)
-        return cropped_img
+input_directory = "./"  
+output_directory = "./"  
 
-# Directory where your images are located
-input_directory = "./centered/"  # Update this with your directory path
-output_directory = "./centered_minified/"  # Update this with your output directory path
+target_size = (80, 80)
 
-# Define the cropping box (left, upper, right, lower)
-crop_box = (291, 32, 1002, 442)  # Example crop box, adjust as needed
-
-# Ensure the output directory exists
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
-# Loop through all files in the input directory
 for filename in os.listdir(input_directory):
-    # Only process image files (you can customize this if needed)
     if filename.lower().endswith(('png', 'jpg', 'jpeg', 'bmp', 'gif')):
-        # Full path to the input image
         input_image_path = os.path.join(input_directory, filename)
 
-        # Crop the image
-        cropped_img = crop_image(input_image_path, crop_box)
+        img = Image.open(input_image_path)
 
-        # Save the cropped image to the output directory
-        output_image_path = os.path.join(output_directory, filename)
-        cropped_img.save(output_image_path)
+        resized_img = img.resize(target_size, Image.ANTIALIAS)
+
+        output_image_path = os.path.join(output_directory, f"{os.path.splitext(filename)[0]}.webp")
+        resized_img.save(output_image_path, format="WEBP", quality=85)
 
         print(f"Cropped image saved: {output_image_path}")
 
